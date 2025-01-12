6d752e82fb20ad72f247bc0f5c30cc9bcdbcf46e
anand <anandvaibhaw000@gmail.com>
2025-01-12 17:15:30 +0530

    replaced create-react-app with create-vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
-// src/App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MenuPage from './pages/MenuPage';
-import OrderPage from './pages/OrderPage';
-import ReservationPage from './pages/ReservationPage';
-import FeedbackPage from './pages/FeedbackPage';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: React.FC = () => (
-  <Router>
-    <div>
-      <Routes>
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/order" element={<OrderPage />} />
-        <Route path="/reservation" element={<ReservationPage />} />
-        <Route path="/feedback" element={<FeedbackPage />} />
-      </Routes>
-    </div>
-  </Router>
-);
+function App() {
+  const [count, setCount] = useState(0)
 
-export default App;
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
