9dae2522fcc742f1c72c118e4e3008439c98a6a0
anand <anandvaibhaw000@gmail.com>
2025-01-17 15:11:10 +0530

    added wallpaper to the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,22 @@
+import image from "../assets/wallpaper1.jpg";
 import Login from "./Login";
 
 const Home = () => {
-
   return (
-    <div className="my-10 px-4 text-center sm:my-16">
-      <h1 className="mb-16 text-3xl sm:text-6xl font-semibold md:text-6xl">
-        The best pizza.
-        <br />
-        <span className="text-orange-500">
-          Straight out of the oven, straight to you.
-        </span>
-      </h1>
+    <div className="relative">
+      <img src={image} alt="wallpaper" className="w-full h-auto" />
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
+        <h1 className="mb-4 text-3xl sm:text-6xl font-semibold md:text-6xl">
+          Magadh Rasam
+          <br />
+            <span className="text-yellow-600 text-2xl sm:text-4xl md:text-4xl">
+            Experience the authentic taste of Magadh with every bite.
+            </span>
+        </h1>
+      </div>
+      <div className="my-10 px-4 text-center sm:my-16">
         <Login />
+      </div>
     </div>
   );
 };
