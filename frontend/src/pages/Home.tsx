f7bbf9d3019570b4471be4a39bb8e490ceb461b9
anand <anandvaibhaw000@gmail.com>
2025-01-19 01:15:19 +0530

    added scroll animation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import image from "../assets/wallpaper1.jpg";
 import Navbar from "../components/Navbar";
 import { FaArrowDown } from "react-icons/fa";
@@ -7,11 +7,18 @@ import Menu from "../components/Menu";
 const Home = () => {
   const [fadeIn, setFadeIn] = useState(false); // State to trigger fade-in effect
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null); // Reference to the Menu section
 
   const handleArrowClick = () => {
-    // Toggle the showMenu state on arrow click
     setShowMenu(true);
+    if (menuRef.current) {
+      menuRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
   };
+
   useEffect(() => {
     // Trigger the fade-in effect after the component mounts
     const timer = setTimeout(() => setFadeIn(true), 500); // 500ms delay
@@ -22,7 +29,7 @@ const Home = () => {
     <>
       <Navbar />
       <div className="relative">
-        <img src={image} alt="wallpaper" className="w-full h-auto" />
+        <img src={image} alt="wallpaper" className="w-full h-screen object-cover" />
         <div className={`absolute inset-0 flex flex-col items-center justify-center text-center text-white ${fadeIn ? 'opacity-100' : 'opacity-0'} transition-opacity duration-1000 ease-in-out`}>
           <h1 className="mb-4 text-3xl sm:text-6xl font-semibold md:text-6xl">
             Magadh Rasam
@@ -35,13 +42,17 @@ const Home = () => {
       </div>
 
       {/* Scroll Down Arrow */}
-      <div className="flex justify-center mt-8">
-      <FaArrowDown
-        className="text-brown-600 animate-bounce w-8 h-8 cursor-pointer"
-        onClick={handleArrowClick}
-      />
-       {/* Conditionally Render Menu */}
-       {showMenu && <Menu />}
+      {!showMenu && (
+        <div className="flex justify-center mt-8">
+          <FaArrowDown
+            className="text-brown-600 animate-bounce w-8 h-8 cursor-pointer"
+            onClick={handleArrowClick}
+          />
+        </div>
+      )}
+
+      <div ref={menuRef}>
+        {showMenu && <Menu />}
       </div>
     </>
   );
