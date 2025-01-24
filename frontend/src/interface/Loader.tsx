bad7b70b1c77a580e7962bcad9afb5c1390abfd9
anand <anandvaibhaw000@gmail.com>
2025-01-24 15:57:51 +0530

    deleted interface dir

--- frontend/src/interface/Loader.tsx
+++
@@ -1,8 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm">
-    </div>
-  );
-};
-
-export default Loader;
