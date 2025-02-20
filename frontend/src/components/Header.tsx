b53c8de2ca2fa6f0fda2110f939d873a30a1e7e1
anand <anandvaibhaw000@gmail.com>
2025-02-20 23:59:46 +0530

    modifies

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -50,7 +50,7 @@ function Header() {
           className="rounded bg-yellow-400 px-4 py-2 font-medium text-white transition duration-150 ease-in-out hover:bg-yellow-600 hover:scale-105"
           onClick={() => navigate('/order')}
         >
-          create order
+          Create order
         </button>
       </div>
 
