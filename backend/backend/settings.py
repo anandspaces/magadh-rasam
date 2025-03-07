981f69d8fe7ed5986ec84a08a920629006453f12
anand <anandvaibhaw000@gmail.com>
2025-03-07 23:17:00 +0530

    updated requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'rest_framework_simplejwt.authentication.JWTAuthentication', # new
     'corsheaders.middleware.CorsMiddleware', # new
 ]
 
