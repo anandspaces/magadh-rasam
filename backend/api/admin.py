65d207427b7bcf1f3a5a9e7b97803bb20401518d
anand <anandvaibhaw000@gmail.com>
2025-07-22 23:45:39 +0530

    registered models in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Menu, Category, Customer, Order, OrderItem, Feedback
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Feedback)
